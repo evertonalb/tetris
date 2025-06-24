e692c80210b84aeaa1b1cf0780e86b2d6c3a172a
Everton Albuquerque <186333879+evertonalb@users.noreply.github.com>
2025-06-24 18:14:09 -0300

    Create main loop with a basic event handling structure

--- main.c
+++ main.c
@@ -3,10 +3,26 @@
 
 int SDL_main(int argc, char *args[]){
 	SDL_Init(SDL_INIT_VIDEO);
+	
 	// Window
     const int width = 800;
-    const int height = 800;
-    SDL_Window *win = SDL_CreateWindow("Snake", width, height, SDL_WINDOW_MAXIMIZED);
+    const int height = 600;
+    SDL_Window *win = SDL_CreateWindow("Tetris", width, height, SDL_WINDOW_MAXIMIZED);
+
+	SDL_Event event;
+	bool running = true;
+	while (running){
+		// Event handling
+		while (SDL_PollEvent(&event)){
+			switch (event.type){
+			case SDL_EVENT_QUIT:
+				running = false;
+				break;
+			default:
+				break;
+			}
+		}
+	}
 
 	return 0;
 }
