a1480cb5b7584a023d091a3af717248c43060e9a
Everton Albuquerque <186333879+evertonalb@users.noreply.github.com>
2025-06-25 20:34:25 -0300

    Fix memory allocation for indices array
    The code was previously allocating only one int when
    it should be allocating a full array in the memory.

--- func.c
+++ func.c
@@ -6,10 +6,11 @@ void polygon_init(Polygon *poly, int n){
 	poly = (Polygon *) malloc(sizeof(Polygon));
 
 	poly->numVertices = n;
-	poly->numIndices = (n - 2) * 3;
+	poly->numIndices  = (n - 2) * 3;
 
 	poly->vertices = (SDL_Vertex *) malloc(sizeof(SDL_Vertex));
-	poly->indices = (int *) malloc(sizeof(int));
+	poly->indices  = (int *) malloc(sizeof(int) * poly->numIndices);
+
 }
 
 void polygon_destroy(Polygon *poly){
